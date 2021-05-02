d0cef8a6d4bc06b003114cce0b7244d5611203a8
IsabelSleigman <sleimusic@gmail.com>
2021-05-02 17:09:21 -0300

    templante drag and drop pronto

---
+++ src/app/drag-and-drop/drag-and-drop.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-and-drop',
+  templateUrl: './drag-and-drop.component.html',
+  styleUrls: ['./drag-and-drop.component.css']
+})
+export class DragAndDropComponent implements OnInit {
+
+  @ViewChild('myrect') myrect: ElementRef;
+
+  top: number = 40;
+  left: number = 300;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
