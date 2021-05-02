8693319c3632e0d4e4598588ea63ca9bc6bf2fff
IsabelSleigman <sleimusic@gmail.com>
2021-05-02 15:28:17 -0300

    Async é muito dhora

--- src/app/async/async.component.ts
+++ src/app/async/async.component.ts
@@ -2,6 +2,11 @@ import { observable, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { map, toArray, delay } from 'rxjs/operators';
 
+interface User {
+  login: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-async',
   templateUrl: './async.component.html',
@@ -10,6 +15,8 @@ import { map, toArray, delay } from 'rxjs/operators';
 export class AsyncComponent implements OnInit {
 
   options$: Observable<string[]>;
+  user$: Observable<User>;
+
 
   constructor() { }
 
@@ -28,9 +35,25 @@ export class AsyncComponent implements OnInit {
     .pipe(
       map(s => s+'!'),
       toArray(),
-      delay(2000)
+      delay(1000)
     );
-    this.options$.subscribe(s => console.log(s));
+    //this.options$.subscribe(s => console.log(s));
+
+    this.user$ = new Observable<User>((observer)=> {
+      let names = ["Isabel", "Felipe", "Marianna", "Gabriel"];
+      let logins = ["isabel", "felipe", "marianna", "gabriel"];
+      let i = 0;
+      console.log("Users")
+      setInterval(()=>{
+        if(i === 4)
+          observer.complete();
+          else {
+            observer.next({login: logins[i], name: names[i]});
+          }
+          i++;
+      }, 3000)
+    })
+    //this.user$.subscribe(s => console.log(s));
   }
 
 }
