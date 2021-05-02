4f5e9eddca3297d7ee8afe5bfbf8666d056206a9
IsabelSleigman <sleimusic@gmail.com>
2021-05-02 17:42:59 -0300

    movimentando o box

--- src/app/drag-and-drop/drag-and-drop.component.ts
+++ src/app/drag-and-drop/drag-and-drop.component.ts
@@ -1,20 +1,50 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-drag-and-drop',
   templateUrl: './drag-and-drop.component.html',
   styleUrls: ['./drag-and-drop.component.css']
 })
-export class DragAndDropComponent implements OnInit {
+export class DragAndDropComponent implements OnInit, AfterViewInit {
 
   @ViewChild('myrect') myrect: ElementRef;
 
   top: number = 40;
-  left: number = 300;
+  left: number = 40;
 
   constructor() { }
 
   ngOnInit(): void {
+   
+
+  }
+
+  ngAfterViewInit(): void {
+
+    let mousedown = fromEvent(this.myrect.nativeElement, 'mousedown');
+    let mousemove = fromEvent(document, 'mousemove');
+    let mouseup = fromEvent(document, 'mouseup');
+  
+    mousedown.subscribe((ed : MouseEvent) => {
+     // console.log(e)
+      let x = ed.pageX;
+      let y = ed.pageY;
+
+      mousemove
+      .pipe(
+        takeUntil(mouseup)
+      ).subscribe((em : MouseEvent) =>{
+        //console.log(em)
+        let offsetx = x = em.pageX;
+        let offsety = y = em.pageY;
+        this.top -= offsety;
+        this.left -= offsetx; 
+        x = em.screenX;
+        y = em.screenY;
+      })
+    })
   }
 
 }
